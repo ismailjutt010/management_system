e2950f0f7acc2a603c86ab87a4254b9455d4c612
Ismail Abdul Kareem <101706647+ismailjutt010@users.noreply.github.com>
2024-08-24 23:28:54 +0500

    -- unit test for dao

---
+++ src/test/java/com/amigoscode/demo/AbstractTestContainer.java
@@ -0,0 +1,51 @@
+package com.amigoscode.demo;
+
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+public abstract class AbstractTestContainerDaoUnitTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Flyway flyway = Flyway.configure().dataSource(
+                postgreSqlContainer.getJdbcUrl(),
+                postgreSqlContainer.getUsername(),
+                postgreSqlContainer.getPassword()
+        ).load();
+        flyway.migrate();
+    }
+
+    @Container
+    private static final PostgreSQLContainer<?> postgreSqlContainer =
+            new PostgreSQLContainer<>("postgres")
+                    .withDatabaseName("amigoscode-dao-unit-test")
+                    .withUsername("amigoscode")
+                    .withPassword("password");
+
+    @DynamicPropertySource
+    private static void registerDataSourceProperties(DynamicPropertyRegistry registry){
+        registry.add(
+                "spring.datasource.url",
+                () -> postgreSqlContainer.getJdbcUrl()
+        );
+
+        registry.add(
+                "spring.datasource.username",
+                () -> postgreSqlContainer.getUsername()
+        );
+
+        registry.add(
+                "spring.datasource.password",
+                () -> postgreSqlContainer.getPassword()
+        );
+    }
+}
