98ee081c2385b9b52d7277f0fcf8e98d08f893aa
Ismail Abdul Kareem <101706647+ismailjutt010@users.noreply.github.com>
2024-09-20 02:55:35 +0500

    --added JWT authentication

---
+++ src/main/java/com/amigoscode/demo/security/SecurityFilterChainConfig.java
@@ -0,0 +1,27 @@
+package com.amigoscode.demo.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityFilterChainConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())  // Disable CSRF protection
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers(HttpMethod.POST, "/api/v1/customers/addCustomer").permitAll()  // Permit POST to /addCustomer
+                        .anyRequest().authenticated()  // All other requests must be authenticated
+                );
+
+        return http.build();
+    }
+}
